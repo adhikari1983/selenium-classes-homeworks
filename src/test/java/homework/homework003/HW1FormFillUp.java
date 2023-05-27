3698cf34b8ed0a0e21de134afc662ebf79fdc6f4
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-05-27 08:58:48 -0400

    Selenium homework 003

---
+++ src/test/java/homework/homework003/HW1FormFillUp.java
@@ -0,0 +1,53 @@
+package homework.homework003;
+
+import classwork.utils.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class HW1FormFillUp extends CommonMethods {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "http://practice.syntaxtechs.net/input-form-demo.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        WebElement firstName = driver.findElement(By.cssSelector("input[name='first_name']"));
+        firstName.sendKeys("Kiran");
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@name='last_name']"));
+        lastName.sendKeys("Adhikari");
+
+        WebElement email = driver.findElement(By.cssSelector("input[placeholder='E-Mail Address']"));
+        email.sendKeys("abc@gmail.com");
+
+        WebElement phone = driver.findElement(By.xpath("//input[@name='phone']"));
+        phone.sendKeys("(703)444-1313");
+
+        WebElement address = driver.findElement(By.cssSelector("input[name='address']"));
+        address.sendKeys("123 Right St");
+
+        WebElement city = driver.findElement(By.xpath("//input[@placeholder='city']"));
+        city.sendKeys("Springfield");
+
+        WebElement state = driver.findElement(By.cssSelector("select[name='state']"));
+        Select selectState = new Select(state);
+        selectState.selectByVisibleText("Virginia");
+
+        WebElement zip = driver.findElement(By.xpath("//input[@name='zip']"));
+        zip.sendKeys("22151");
+
+        WebElement websiteDomain = driver.findElement(By.cssSelector("input[placeholder='Website or domain name']"));
+        websiteDomain.sendKeys("practiceAllDay.com");
+
+        WebElement radioBtn = driver.findElement(By.xpath("//input[@value='yes']"));
+        radioBtn.click();
+
+        WebElement projectDescription = driver.findElement(By.cssSelector("textarea[name='comment']"));
+        projectDescription.sendKeys("Selenium homework");
+
+        WebElement sendBtn = driver.findElement(By.xpath("//button[@onclick='resetForm();']"));
+        sendBtn.click();
+
+        closeBrowser();
+    }
+}
---
+++ src/test/java/homework/homework003/HW2VerifyErrorMessage.java
@@ -0,0 +1,32 @@
+package homework.homework003;
+
+import classwork.utils.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HW2VerifyErrorMessage extends CommonMethods {
+
+    public static void main(String[] args) {
+        String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login%E2%80%9D";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        WebElement userName = driver.findElement(By.xpath("//input[@id='txtUsername']"));
+        userName.sendKeys("kiran123");
+
+        WebElement logInBtn = driver.findElement(By.xpath("//input[@id='btnLogin']"));
+        logInBtn.click();
+
+        WebElement errorMessage = driver.findElement(By.xpath("//span[@id='spanMessage']"));
+        String errorMessageText = errorMessage.getText();
+
+        if (errorMessageText.equalsIgnoreCase("Password cannot be empty")) {
+            System.out.println("Right error message is displayed");
+        } else {
+            System.out.println("Error message is not displayed");
+        }
+
+        closeBrowser();
+
+    }
+}
