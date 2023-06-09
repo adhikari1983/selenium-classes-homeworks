61003022cacb97c053ffb065e1689ddca265e3b2
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-06-09 12:02:10 -0400

    class 008 - homework

---
+++ src/test/java/homework/homework008/HW.java
@@ -0,0 +1,61 @@
+/*
+hw:
+    goto syntax hrms
+    http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login
+    login
+    click on recruitment
+    select a date on the calendar
+ */
+
+package homework.homework008;
+
+import homework.utils.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class HW extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        //login to syntax hrms
+        WebElement userName = driver.findElement(By.xpath("//input[@name='txtUsername']"));
+        userName.sendKeys("Admin");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@name='txtPassword']"));
+        userPassword.sendKeys("Hum@nhrm123");
+        WebElement logInBtn = driver.findElement(By.xpath("//input[contains(@id, 'btn')]"));
+        logInBtn.click();
+
+        // click on recruitment btn
+        WebElement recruitmentBtn = driver.findElement(By.xpath("//a[@id='menu_recruitment_viewRecruitmentModule']"));
+        recruitmentBtn.click();
+
+        // click on from calendar
+        WebElement fromCalenderBtn = driver.findElement(By.xpath("(//img[@class='ui-datepicker-trigger'])[1]"));
+        fromCalenderBtn.click();
+
+        //select the month
+        WebElement month = driver.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+        Select selectMonth = new Select(month);
+        selectMonth.selectByVisibleText("Aug");
+        selectMonth.equals("Aug");
+
+        //select the year
+        WebElement year = driver.findElement(By.xpath("//select[@class='ui-datepicker-year']"));
+        Select selectYear = new Select(year);
+        selectYear.selectByVisibleText("2025");
+
+        //select the day
+        List<WebElement> allDates = driver.findElements(By.xpath("//*[@id=\"ui-datepicker-div\"]/table/tbody/tr/td"));
+        for (WebElement date : allDates) {
+            String currentDate = date.getText();
+            if (currentDate.equals("8")) {
+                date.click();
+            }
+        }
+    }
+}
